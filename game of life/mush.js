57380b40e2def548587775a8708e8e8a5b63742d
ApetHak2008 <habetnak.h@tumo.org>
2022-02-12 10:56:24 +0400

    inheritance

--- game of life/mush.js
+++ game of life/mush.js
@@ -1,18 +1,5 @@
-class Mush {
+class Mush extends LivingCreature{
     constructor(x, y) {
-        this.x = x
-        this.y = y
-        this.multiply = 0
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-
+    super(x,y)
     }
 }
