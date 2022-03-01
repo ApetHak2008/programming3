4a509e76cc80958bbeecd58eda6c898cb1f7769e
ApetHak2008 <habetnak.h@tumo.org>
2022-03-01 19:21:42 +0400

    add

--- game of life/mush.js
+++
@@ -1,7 +0,0 @@
-let LivingCreature = require('./LivingCreature')
-
-module.exports  = class Mush extends LivingCreature{
-    constructor(x, y) {
-    super(x,y)
-    }
-}
--- game of life/poison.js
+++
@@ -1,7 +0,0 @@
-let LivingCreature = require('./LivingCreature')
-
-module.exports  = class Poison extends LivingCreature{
-    constructor(x, y) {
-    super(x,y)
-    }
-}
